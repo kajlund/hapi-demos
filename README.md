346009692b4f454170b09fddb27d04784136880c
Kaj Lund <kaj.lund@gmail.com>
2019-01-13 10:03:52 +0200

    Add cookie-auth  demo

--- README.md
+++ README.md
@@ -9,8 +9,17 @@ npm i -D eslint eslint-config-prettier eslint-plugin-prettier eslint-watch prett
 Small tweaks in .prettierrc and .eslintrc.json. The .editorconfig set for 2-space-indent anf lf linefeeds. Many projects will use `getconfig` to load env variables and configs.
 
 
-## Samples
+## Authorization
 
-### Authorization
+Authentication in hapi is based on **schemes** and **strategies**. A scheme is the type of authentication used, like basic or bearer whereas a strategy is a configured and named instance of a particular scheme: `server.auth.strategy(name, scheme, [mode], options)`.
 
-* Auth Basic sample at `src/auth/basic`
+You can register multiple strategies for the same server. A strategy can apply to all routes or be set separately for each route via the route config object.
+
+### Basic
+
+Basic authorization sends username and password in the header with every request and therefore should not be used in general. With Hapi you can use [hapi-auth-basic](https://github.com/hapijs/hapi-auth-basic) a basic authentication plugin. A demo app using basic auth can be found in `src/auth/basic/index.js`.
+
+### Cookie-Based
+
+For cookie auth you can use the [hapi-auth-cookie](https://github.com/hapijs/hapi-auth-cookie) plugin. You can find a demo app in `src/auth/cookie/index.js`. The cookie scheme can, but is not obligated to, have a validate method. That method could be used for disallowing users with valid cookies if need be. Hapi's `request.auth` object provides the additional boolean attribute `isAuthenticated`. If isAuthenticated the `request.auth.credentials` will typically contain user data.
+
---
+++ src/auth/cookie/index.js
@@ -0,0 +1,120 @@
+"use strict"
+
+const Bcrypt = require("bcryptjs")
+const Boom = require("boom")
+const Glue = require("glue")
+const { User } = require("../../users")
+
+const manifest = {
+  server: {
+    host: "localhost",
+    port: 3000
+  },
+  register: {
+    plugins: [{ plugin: require("hapi-auth-cookie") }]
+  }
+}
+
+const main = async () => {
+  try {
+    const server = await Glue.compose(
+      manifest,
+      { relativeTo: __dirname }
+    )
+
+    server.auth.strategy("session", "cookie", {
+      password: "password-should-be-32-characters",
+      cookie: "sid-example",
+      // redirectTo: '/login',
+      isSecure: false
+    })
+
+    server.route({
+      method: "GET",
+      path: "/public",
+      config: {
+        auth: false,
+        handler: () => {
+          return { message: "The public route is for everybody" }
+        }
+      }
+    })
+
+    server.route({
+      method: "GET",
+      path: "/private",
+      config: {
+        auth: { mode: "try", strategy: "session" },
+        handler: request => {
+          if (request.auth.isAuthenticated) {
+            // session data available
+            // const session = request.auth.credentials
+            return { message: `Welcome ${request.auth.credentials.username}! You have access to our private data` }
+          }
+          return { message: "The private route is for authenticated users only" }
+        }
+      }
+    })
+
+    server.route({
+      method: "POST",
+      path: "/login",
+      config: {
+        handler: async request => {
+          let user
+
+          if (request.auth.isAuthenticated) {
+            return { message: "Yo! You dawg already authenticated." }
+          }
+
+          const username = request.payload.username
+          const pwd = request.payload.password
+
+          try {
+            // check if user exists in DB
+            user = await User.findOne({ username })
+            if (!user) {
+              throw new Error(`Invalid username ${username} tried to logon`)
+            }
+
+            // compare passwords
+            const isValid = await Bcrypt.compare(pwd, user.password)
+            if (!isValid) {
+              throw new Error(`User ${username} tried to logon with invalid pwd: ${pwd}`)
+            }
+
+            const { password, ...credentials } = user // remove pwd from User obj
+            request.cookieAuth.set(credentials)
+          } catch (err) {
+            console.error(err.stack)
+            throw Boom.unauthorized()
+          }
+
+          return { message: `Welcome ${user.username}` }
+        }
+      }
+    })
+
+    server.route({
+      method: "GET",
+      path: "/logout",
+      config: {
+        auth: "session",
+        handler: request => {
+          // clear the session data
+          request.cookieAuth.clear()
+          return { message: "User logged out" }
+        }
+      }
+    })
+
+    console.log("Starting server")
+    await server.start()
+    console.log(`Server running at: ${server.info.uri}`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+main()
